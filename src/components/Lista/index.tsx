9d38c8e3705157d1d4af37e629bc2b19fb5c35ab
Gabriel Barros <super.gcb18@gmail.com>
2022-02-25 19:53:46 -0300

    Initial commit

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,24 @@
+import Item from "./Item";
+import style from "./Lista.module.scss"
+import { ITarefa } from "../../types/tarefa";
+
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+
+  return (
+    <aside className={style.listaTarefas}>
+      <h2>Estudos do dia</h2>
+      <ul>
+        {tarefas.map((item) => (
+          <Item {...item} key={item.id} selecionaTarefa={selecionaTarefa} />
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export default Lista;
